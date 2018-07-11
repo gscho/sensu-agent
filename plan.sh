ac3eb80e14dab7619ae81a43ab3a1673c216ea84
gscho <greg.c.schofield@gmail.com>
2018-07-11 15:53:27 -0400

    Refactor to use pre-built binary

--- plan.sh
+++ plan.sh
@@ -1,39 +1,30 @@
 go_pkg="github.com/sensu/sensu-go"
 pkg_name=sensu-agent
+pkg_filename=sensu-agent
 pkg_origin=gscho
 pkg_version="2.0.0-beta.2-4"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
-pkg_source="https://$go_pkg"
-pkg_scaffolding=core/scaffolding-go
+pkg_source="https://storage.googleapis.com/sensu-binaries/$pkg_version/linux/amd64/$pkg_name"
 pkg_svc_run="sensu-agent start -c $pkg_svc_config_path/agent.yml"
-pkg_shasum="54b93c8e54fd07dc90e7a0c840e5d93c43055499f612325c6d94d8f66f7e42e9"
-pkg_build_deps=(core/which)
+pkg_shasum="5825044fa69edc27fc74769001076c716a855a82103224727438f3ec0bbe8f90"
+pkg_build_deps=(core/go)
 pkg_bin_dirs=(bin)
 pkg_binds_optional=(
-  [sensu-backend]="port"
+  [backend]="port"
 )
-
 pkg_description="Sensu 2.0 Agent"
 pkg_upstream_url="https://sensu.io"
 
-do_prepare() {
-  mkdir -p "$scaffolding_go_pkg_path/bin"
-  export PATH=$scaffolding_go_pkg_path/bin:$PATH
+do_unpack(){
+  return 0
 }
 
-do_download() {
-  # We need to override the scaffold's default download because the agent
-  # is in a subdir of the package repository
-  go get -d $go_pkg 2>&1 | grep -q "no Go files"
-  cd $scaffolding_go_pkg_path && git reset --hard $pkg_version
-}
-
-do_build() {
-  ln -fs "$(which env)" /usr/bin/env
-  cd $scaffolding_go_pkg_path && ./build.sh build_agent
+do_build(){
+  return 0
 }
 
 do_install() {
-  cp -f ${scaffolding_go_pkg_path:?}/bin/sensu-agent $pkg_prefix/bin
+  chmod +x $HAB_CACHE_SRC_PATH/$pkg_filename
+  cp -f $HAB_CACHE_SRC_PATH/$pkg_filename $pkg_prefix/bin
 }
